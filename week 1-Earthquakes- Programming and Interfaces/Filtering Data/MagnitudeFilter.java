b5bac23eabc2a6d7b746c38dc7aa6a57a0eff1ca
x239242 <MoMo@789>
2018-12-27 15:31:04 -0600

    Coursera week 1 Assignment 1: Implementing Filters

---
+++ week 1-Earthquakes- Programming and Interfaces/Filtering Data/MagnitudeFilter.java
@@ -0,0 +1,27 @@
+public class MagnitudeFilter implements Filter {
+
+
+    private double minMag;
+    private double maxMag;
+    private String name ;
+
+    public MagnitudeFilter(double num1, double num2){
+
+        minMag = num1;
+        maxMag = num2;
+        name = "MagnitudeFiter" ;
+
+    }
+
+    public boolean satisfies(QuakeEntry qe){
+        double mag = qe.getMagnitude();
+        if (mag >= minMag && mag <= maxMag){
+            return true;
+        }
+        return false;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
